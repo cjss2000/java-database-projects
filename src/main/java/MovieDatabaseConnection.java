f16737c5f55f276cf44f4d25dec3865d1badd5c2
cjss2000 <cjss2000@gmail.com>
2025-06-09 11:01:53 +0200

    created new class for connection to mysql

---
+++ src/main/java/MovieDatabaseConnection.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MovieDatabaseConnection {
+
+    public static void main(String[] args) {
+
+    }
+}
