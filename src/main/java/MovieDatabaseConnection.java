ae7f0c8e512f47910e2aff524a318444173c00c5
cjss2000 <cjss2000@gmail.com>
2025-06-09 11:21:22 +0200

    created new class for connection to mysql

--- src/main/java/MovieDatabaseConnection.java
+++ src/main/java/MovieDatabaseConnection.java
@@ -6,7 +6,28 @@ import java.sql.Statement;
 
 public class MovieDatabaseConnection {
 
-    public static void main(String[] args) {
+    private static String URL = "jdbc:mysql://localhost:3306/demo_db";
+    private static String USERNAME = "root";
+    private static String PASSWORD = "root";
+
+
+    public static void main(String[] args) throws  SQLException {
+
+        Connection customConnection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+
+        String COMMAND = "select * from movie;";
+
+        try (Statement statement = customConnection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(COMMAND);
+
+            while (resultSet.next()) {
+                String movie_id = resultSet.getString("movie_id");
+                System.out.println("Movie ID" + movie_id);
+            }
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+        }
 
     }
 }
