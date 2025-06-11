ddad0fc03ae4da29dbc5c5485489db5cd137b70d
cjss2000 <cjss2000@gmail.com>
2025-06-11 19:03:26 +0200

    created new class for connection to mysql

--- src/main/java/MovieDatabaseConnection.java
+++ src/main/java/MovieDatabaseConnection.java
@@ -3,6 +3,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
+
 
 public class MovieDatabaseConnection {
 
@@ -12,6 +14,8 @@ public class MovieDatabaseConnection {
 
 
     public static void main(String[] args) throws  SQLException {
+
+      //  Scanner scanner = new Scanner(System.in);
 
         Connection customConnection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
 
